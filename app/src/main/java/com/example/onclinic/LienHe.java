113faa600632708d3025268d980960e2c831c67e
Xuân <levanxuan5518@gmail.com>
2021-10-15 20:03:46 +0700

    push file lienhe anddatphong

---
+++ app/src/main/java/com/example/onclinic/LienHe.java
@@ -0,0 +1,14 @@
+package com.example.onclinic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LienHe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lien_he);
+    }
+}
