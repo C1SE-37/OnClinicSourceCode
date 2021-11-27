f52a66f5acad5b7a407b7672a72406f29567ae8d
Xuân <levanxuan5518@gmail.com>
2021-11-27 21:18:27 +0700

    update video call

--- app/src/main/java/com/example/onclinic/VideoCall.java
+++ app/src/main/java/com/example/onclinic/VideoCall.java
@@ -1,13 +1,54 @@
 package com.example.onclinic;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import org.jitsi.meet.sdk.JitsiMeet;
+import org.jitsi.meet.sdk.JitsiMeetActivity;
+import org.jitsi.meet.sdk.JitsiMeetConferenceOptions;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
 
 public class VideoCall extends AppCompatActivity {
+    Button btnJoin;
+    EditText edtSecrectCode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_call);
+
+        btnJoin = findViewById(R.id.btnJoin);
+        edtSecrectCode = findViewById(R.id.edtSecrectCode);
+
+        URL server;
+        try{
+            server =new URL("https://meet.jit.si");
+            JitsiMeetConferenceOptions defaultOptions=
+                    new JitsiMeetConferenceOptions.Builder()
+                            .setServerURL(server)
+                            .setWelcomePageEnabled(false)
+                            .build();
+            JitsiMeet.setDefaultConferenceOptions(defaultOptions);
+
+        }catch (MalformedURLException e){
+            e.printStackTrace();
+        }
+        btnJoin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                JitsiMeetConferenceOptions options = new JitsiMeetConferenceOptions.Builder()
+                        .setRoom(edtSecrectCode.getText().toString())
+                        .setWelcomePageEnabled(false).build();
+                JitsiMeetActivity.launch(VideoCall.this,options);
+            }
+        });
+
     }
 }
